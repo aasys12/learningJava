dfb1d9c53af167acf3b6abb1ab7a2fb113c3a5d5
Aashish Sharma <aasyspoudyal@gmail.com>
2018-06-15 13:40:53 -0500

    Static Method

---
+++ src/FutureValueApp.java
@@ -0,0 +1,52 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+public class FutureValueApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Future Value App\n");
+        Scanner sc=new Scanner(System.in);
+        String choice="y";
+        while (choice.equalsIgnoreCase("y")){
+            //get the input from the user
+            System.out.print("Enter monthly investment: ");
+            double monthlyInvestment =sc.nextDouble();
+            System.out.print("Enter yearly investment rate: ");
+            double interestRate =sc.nextDouble();
+            System.out.print("Enter number of years: ");
+            int years=sc.nextInt();
+
+            //convert yearly values to monthly values
+            double monthlyInterestRate=interestRate/12/100;
+            int months=years/12;
+
+            //call future value method
+
+            double futureValue =calaulateFutureValue(monthlyInvestment,monthlyInterestRate,months);
+
+            //format and display the result
+
+            NumberFormat currency = NumberFormat.getCurrencyInstance();
+            System.out.println("Future value:  "+
+            currency.format(futureValue));
+            System.out.println();
+
+            //see if the user wants to continue
+            System.out.print("Continue? (y/n): ");
+            choice=sc.next();
+
+            System.out.println();
+
+
+        }
+    }
+
+    //static method that requires three arguments and return a double
+    public static double calaulateFutureValue(double monthlyInvestment,
+                                              double monthlyInterestRate, int months){
+        double futureValue=0.0;
+        for (int i = 0; i <months ; i++) {
+            futureValue=(futureValue+monthlyInvestment)*(1+monthlyInterestRate);
+
+        }
+        return futureValue;
+    }
+}
