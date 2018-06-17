086d8d25754b14c976f2562a5ac900a6ff955a96
Aashish Sharma <aasyspoudyal@gmail.com>
2018-06-16 23:58:38 -0500

    Objects

---
+++ src/CarMain.java
@@ -0,0 +1,9 @@
+public class CarMain {
+    public static void main(String[] args) {
+        Car porsche=new Car();
+        Car Holden=new Car();
+        porsche.setModels("carrera");
+        System.out.println("Model is: "+porsche.getModels());
+    }
+
+}
