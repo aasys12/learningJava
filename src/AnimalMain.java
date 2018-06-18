4fc386732c01ec10d461628f3e215df290962551
Aashish Sharma <aasyspoudyal@gmail.com>
2018-06-17 23:28:36 -0500

    Super class and Inheritance

---
+++ src/AnimalMain.java
@@ -0,0 +1,7 @@
+public class AnimalMain {
+    public static void main(String[] args) {
+        Animal animal=new Animal("Animal",1,1,1,1);
+        Dog dog=new Dog("Yorkie",7,30,2,4,1,20,"Silky");
+        dog.eat();
+    }
+}
